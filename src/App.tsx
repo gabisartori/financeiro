ed5269d69196f2eee9ab8cc19639f8e16c8817d0
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-08-01 16:45:42 -0300

    começando a lidar com lista de transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Form from './components/Form';
 import Header from './components/Header/index';
 import Summary from './components/Summary';
 import GlobalStyle from './styles/global';
 
 function App() {
+  const data = localStorage.getItem('entradas');
+  const [entradas, setEntradas] = useState(data ? JSON.parse(data) : []);
+
+  const [receitas, setReceitas] = useState(0);
+  const [despesas, setDespesas] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  // useEffect(() => {
+  //   const totalReceitas = entradas.filter((item:)=>!item.despesa)
+
+
   return (
     <div>
       <GlobalStyle />
